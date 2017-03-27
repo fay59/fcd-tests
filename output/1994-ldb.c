0d4138381493169c5d39107dbd6129eba8a20506
Travis CI <travis@zneak.github.io>
2017-03-27 08:17:13 +0000

    Test results on linux for fcd commit 1ddcb4e1f643d1b2ce7f7fdf0fd007bab26c6dc2

---
+++ output/1994-ldb.c
@@ -0,0 +1,93 @@
+#include "tests/bin/1994-ldb.h"
+
+void _init(uint64_t arg0)
+{
+	if (*(uint64_t*)0x600a68 != 0)
+	{
+		__gmon_start__();
+	}
+	return;
+}
+void main(uint64_t arg0)
+{
+	uint32_t phi3;
+	do
+	{
+		while (true)
+		{
+			uint32_t* anon2;
+			uint32_t* anon1 = (uint32_t*)0x600d34;
+			if (*anon1 == 0)
+			{
+				*anon1 = 1;
+				anon2 = (uint32_t*)0x600d38;
+				srand((uint32_t)time((uint64_t*)(__zext uint64_t)*anon2));
+				phi3 = *anon1;
+				break;
+			}
+			else 
+			{
+				uint32_t anon4 = rand();
+				uint32_t anon5 = *anon1 + 1;
+				*anon1 = anon5;
+				uint64_t phi6 = 6294568;
+				if ((uint32_t)((__sext int64_t)anon4 % (__sext int64_t)*anon1) == 0)
+				{
+					phi6 = ((__sext int64_t)anon5 << 2) + 6294336;
+				}
+				if ((uint32_t)(uint64_t)gets((uint8_t*)phi6) == 0)
+				{
+					phi3 = puts((uint8_t*)(((__sext int64_t)*anon2 << 2) + 6294336)) & *anon2;
+					break;
+				}
+			}
+		}
+	}
+	while (phi3 != 0);
+	return;
+}
+void _start(uint64_t arg0, uint64_t arg1, uint64_t arg2)
+{
+	*(uint64_t*)(arg1 - 8) = arg0;
+	uint64_t anon2 = arg1 & 0xfffffffffffffff0;
+	uint64_t anon1 = anon2 - 16;
+	*(uint64_t*)anon1 = anon2 - 8;
+	__libc_start_main((uint32_t(*)(uint32_t, uint8_t**, uint8_t**))0x400520, (uint32_t)arg0, (uint8_t**)arg1, (void(*)())0x4006d0, (void(*)())0x400740, (void(*)())arg2, *(uint8_t**)anon1);
+	__builtin_trap();
+}
+void deregister_tm_clones(uint64_t arg0)
+{
+	return;
+}
+void register_tm_clones(uint64_t arg0)
+{
+	return;
+}
+void __do_global_dtors_aux(uint64_t arg0)
+{
+	uint8_t* anon1 = (uint8_t*)0x600b00;
+	if (*anon1 == 0)
+	{
+		deregister_tm_clones(4195986);
+		*anon1 = 1;
+	}
+	return;
+}
+void frame_dummy(uint64_t arg0)
+{
+	return;
+}
+void __libc_csu_init(uint64_t arg0, uint64_t arg1, uint64_t arg2, uint64_t arg3)
+{
+	_init(4196099);
+	(*(void(**)(uint64_t, uint64_t, uint64_t))0x600880)(arg1 & 0xffffffff, arg2, arg3);
+	return;
+}
+void __libc_csu_fini(uint64_t arg0)
+{
+	return;
+}
+void _fini(uint64_t arg0)
+{
+	return;
+}
